68acd40f0bca8348151f3d63b89a6cca63a9a360
2-Ralph <thorbar@icloud.com>
2021-08-18 18:21:59 +0200

    updated files

--- react-clock-working/src/App.js
+++ react-clock-working/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -122,51 +122,33 @@ function App(){
   const [pauseIcon, setPauseIcon] = useState(false);
   const [formattedTimer, setFormattedTimer] = useState('25 : 00');
   const [pauseState, setPauseState] = useState(true);
-  let timer;
-  
+  //const [ms, setMs] = useState(sessionVal*1000*60);
+  let ms = sessionVal*1000*60;
+  const interval = useRef(null)
+
   useEffect(() => {
-    setFormattedTimer(`${sessionVal} : 00`)
-  }, [sessionVal]);
+    setFormattedTimer(`${sessionVal} : 00`);
+    ms = sessionVal*1000*60;
+  }, [sessionVal])
 
-  function handleCountDown(ms=sessionVal*60*1000){
-    let startTime, timer, objMethods = {};
+  function handleCountDown(){
+    console.log("btn onclick VAL:", Math.floor(ms / 1000) % 60)
 
-    objMethods.resume = function () {
-      startTime = new Date().getTime();
-      timer = setInterval(objMethods.step, 200);
+    const countDown = () => {
+      ms -= 1000;
+      let m = Math.floor(ms / 60000);
+      let s = Math.floor(ms / 1000) % 60;
+      console.log("countdown func run VAL:", s);
+      setFormattedTimer(`${m} : ${s}`);
     };
-    objMethods.pause = function () {
-      ms = objMethods.step();
-      console.log(ms);
-      setFormattedTimer('lol')
-      clearInterval(timer);
-
-    };
-    objMethods.step = function () {
-      let now = Math.max(0, ms - (new Date().getTime() - startTime))
-      let m = Math.floor(now / 60000);
-      let s = Math.floor(now / 1000) % 60;
-      s = (s < 10 ? "0" : "") + s;
-      setFormattedTimer(`${m} : ${s}`)
-      return now;
-    };
-    objMethods.resume();
-    return objMethods;
-  }
-
-
-
-  function handlePause(){
-    timer = handleCountDown();
-    console.log('handlePause entered')
-    if(pauseState) {
-      console.log('pausestate true')
-      timer.resume();
+    const initInterval = () => {
+      interval.current = setInterval(countDown, 1000)
     }
-    else {
-      console.log('pausestate false');
-      timer.pause();
-    }
+    const pause = () => {
+      clearInterval(interval.current);
+    };
+
+    pauseState ? initInterval() : pause();
     setPauseState(prev => !prev);
   }
 
@@ -210,146 +192,12 @@ function App(){
         setBreakVal(5);
         setSessionVal(25);
       }}
-      handleCountDown={handlePause}
+      handleCountDown={handleCountDown}
     />
     </div>
-    
-  )
+  );
+};
 
-}
-/*
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      breakVal: 5,
-      sessionVal: 25,
-      iconToggleState: 'fa fa-pause',
-      currentTimerState: 'session',
-      formattedTimer: '25 : 00',
-      timerActive: false
-    }
-    this.handleIncrementAndDecrement = this.handleIncrementAndDecrement.bind(this);
-    this.handleIconChange = this.handleIconChange.bind(this);
-    this.handleCountDown = this.handleCountDown.bind(this);
-    this.handleRestartClick = this.handleRestartClick.bind(this);
-    this.handlePause = this.handlePause.bind(this);
-  };
-
-  
-  handleIconChange() {
-    if (this.state.iconToggleState == 'fa fa-pause') {
-      this.setState({
-        iconToggleState: "fa fa-play"
-      })
-    } else {
-      this.setState({
-        iconToggleState: 'fa fa-pause'
-      })
-    };
-  };
-
-  handleRestartClick() {
-    this.setState({
-      breakVal: 5,
-      sessionVal: 25,
-      iconToggleState: 'fa fa-pause',
-      currentTimerState: 'session',
-      formattedTimer: '25 : 00'
-    })
-  }
-
-  handleCountDown(ms = this.state.sessionVal * 1000 * 60) {
-    let that = this;
-    let startTime, timer, objMethods = {};
-
-    objMethods.resume = function () {
-      startTime = new Date().getTime();
-      timer = setInterval(objMethods.step, 200);
-    };
-    objMethods.pause = function () {
-      ms = objMethods.step();
-      clearInterval(timer);
-    };
-    objMethods.step = function () {
-      let now = Math.max(0, ms - (new Date().getTime() - startTime))
-      let m = Math.floor(now / 60000);
-      let s = Math.floor(now / 1000) % 60;
-      s = (s < 10 ? "0" : "") + s;
-      that.setState({
-        formattedTimer: `${m} : ${s}`
-      });
-      return now;
-    };
-    objMethods.resume();
-    return objMethods;
-  };
-
-  handlePause() {
-    let timer = this.handleCountDown();
-    if (this.state.timerActive) {
-      timer.pause();
-      console.log('pause entered');
-    } else {
-      timer.resume();
-      console.log('resume entered');
-    }
-    this.setState({
-      timerActive: !this.state.timerActive
-    });
-  }
-
-
-
-  handleIncrementAndDecrement(e) {
-    if (this.state.breakVal === 0 && /dec/i.test(e.target.value) || this.state.sessionVal === 0 && /dec/i.test(e.target.value)) {
-      return;
-    }
-
-    switch (e.target.value) {
-      case 'sesh-inc':
-        this.setState({
-          sessionVal: this.state.sessionVal += 1
-        });
-        break;
-      case 'sesh-dec':
-        this.setState({
-          sessionVal: this.state.sessionVal -= 1
-        });
-        break;
-      case 'break-inc':
-        this.setState({
-          breakVal: this.state.breakVal += 1
-        });
-        break;
-      case 'break-dec':
-        this.setState({
-          breakVal: this.state.breakVal -= 1
-        });
-        break;
-      default:
-        return;
-    }
-  }
-
-  render() {
-    return (
-      <div id="wrapper">
-        <TimeDisplay
-          handleIconChange={this.handleIconChange}
-          iconToggleState={this.state.iconToggleState}
-          breakVal={this.state.breakVal}
-          sessionVal={this.state.sessionVal}
-          handleIncrementAndDecrement={this.handleIncrementAndDecrement}
-          handleRestartClick={this.handleRestartClick}
-          formattedTimer={this.state.formattedTimer}
-          handleCountDown={this.handlePause}
-        />
-      </div>
-    )
-  }
-}
-*/
 export default App
 
 ReactDOM.render(
